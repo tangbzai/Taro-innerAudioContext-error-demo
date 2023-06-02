ce0a020f6eb41b21ed96a280d700ae6032c3b823
tangbzai <tan-jc@qq.com>
2023-06-02 11:23:48 +0800

    feat: 复现资源请求错误

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import Taro, { useLoad } from '@tarojs/taro'
+import { useRef } from 'react'
 import './index.less'
 
 export default function Index() {
+  const audio = useRef(Taro.createInnerAudioContext())
 
   useLoad(() => {
     console.log('Page loaded.')
+    audio.current.onError(console.log)
+    // 找一个会请求失败的路径
+    audio.current.src = 'localhost:12312/errorLoadPath'
+    audio.current.autoplay = true
   })
 
   return (
